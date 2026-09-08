c4e3c3211c989b520c64efb648cd96ef3bfeb371
irfanxzone <irfanxzone121@gmail.com>
2026-09-08 20:17:30 +0500

    Redesign iPhone page and refresh publication dates

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -34,7 +34,7 @@ const JSON_LD = {
       "isPartOf": {
         "@id": "https://tiktokdownloader.ph/#website"
       },
-      "dateModified": "2026-09-07T15:51:53+08:00"
+      "dateModified": "2026-09-08T23:16:26+08:00"
     },
     {
       "@type": "BreadcrumbList",
